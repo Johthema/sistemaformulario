0ce59ef6318b5f102481894e48bb02b218988c87
Johthema <mauriciojodi@gmail.com>
2023-11-08 11:53:15 -0400

    Atualiza versao insercao e testes de componentes

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,13 +2,23 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import '@radix-ui/themes/styles.css';
+import { Theme } from '@radix-ui/themes';
+import Login from '../pages/login/index'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <>
-    <h2>Projeto piloto - formulários</h2>
+     <html>
+      <body>
+        <Theme>
+          <Login/>
+        </Theme>
+      </body>
+    </html>
+
     </>
   )
 }
