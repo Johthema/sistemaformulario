dde86181e6b37e277c68e61f303bb74a4870fe52
Johthema <mauriciojodi@gmail.com>
2023-11-10 15:50:51 -0400

    Atualiza versao login

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,11 +1,13 @@
 // import { Flex, Text, Button, Card, Inset, Strong } from '@radix-ui/themes';
 import Style from './login.module.css'
-import { FaBeer } from 'react-icons/fa';
+import { FaBeer, FaLock } from 'react-icons/fa';
 import {useState, useEffect} from 'react'
 import { useRouter } from "next/router";
-
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import Form from 'react-bootstrap/Form';
+
 
 export default function Login(){
   const router = useRouter();
@@ -32,7 +34,7 @@ export default function Login(){
           <div className={Style.divCard}>
             <div className={Style.divFormEsq}>
               <div className={Style.cardFormEsq}>
-                <h1>SiCoDa - sistema de Coleta de Dados</h1>
+                <h1><b className={Style.tituloLogo}>SiCoDa</b> - sistema de Coleta de Dados</h1>
               </div>
                 
             </div>
@@ -44,12 +46,31 @@ export default function Login(){
                <Card className={transicao ? Style.cardFormDir : Style.cardFormDir2  }>
                 
                 <Card.Body>
-                  <Card.Title>Card Title</Card.Title>
-                  <Card.Text>
-                    Some quick example text to build on the card title and make up the
-                    bulk of the card's content.
+                  <Card.Title><FaLock className={Style.iconeCadeado} /></Card.Title>
+                  <div className={Style.divTituloCardLogin}>
+                      <h2 className={Style.titulologin}>Quem é você?</h2>
+                      <h4 className={Style.subtitulologin}>Se identifique para fazer login</h4>
+                  </div>
+                  <hr></hr>
+                  <Card.Text className={Style.entradaSenhaEmail}>
+
+                  <FloatingLabel
+                    controlId="floatingInput"
+                    label="Email"
+                    className="mb-3">
+                    <Form.Control type="email" placeholder="name@example.com" />
+                  </FloatingLabel>
+                  <FloatingLabel controlId="floatingPassword" label="Senha">
+                    <Form.Control type="password" placeholder="Senha" />
+                  </FloatingLabel>
+                   <div className={Style.divEsqueciSenha}>
+                      <span className={Style.spanEsquecisenha}>Esqueci senha</span>
+                   </div>
                   </Card.Text>
-                  <Button variant="primary" onClick={Logar}>Entrar</Button>
+                  <div className={Style.divBotaoEntrar}>
+                    <Button variant="primary" onClick={Logar} className={Style.botaoEntrar}>Entrar</Button>
+                  </div>
+                  
                 </Card.Body>
               </Card>
             </div>
