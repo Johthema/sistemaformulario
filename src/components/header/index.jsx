fb6aa030758944c8db963ea508e05c940fbb62d0
Johthema <mauriciojodi@gmail.com>
2023-11-13 13:05:33 -0400

    Atualiza versao com logo

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -13,6 +13,9 @@ import Dropdown from 'react-bootstrap/Dropdown';
 import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 import Tooltip from 'react-bootstrap/Tooltip';
 import { useRouter } from "next/router";
+import Image from 'next/image';
+import Logo from '../../assets/logo_pequena.png';
+
 
 export default function Header(){
   const router = useRouter();
@@ -20,35 +23,29 @@ export default function Header(){
     
   }
 
+  function Home(){
+    
+    router.push('/home')
+   
+    
+  } 
+
 
     return(
         <>
             <Navbar expand="lg" className={Style.headerNavBar}>
       <Container fluid>
-        <Navbar.Brand href="/home" className={Style.headerIconeLogo}>Nome do usuario logado</Navbar.Brand>
+        {/* <Navbar.Brand className={Style.headerIconeLogo}>Nome do usuario logado</Navbar.Brand> */}
+        <h5 className={Style.headerIconeLogo}>Nome do usuario logado</h5>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
-          {/* <Nav
-            className="me-auto my-2 my-lg-0"
+          <Nav
+            className="me-auto my-2 my-lg-0 justify-content-center flex-grow-1 pe-3"
             style={{ maxHeight: '100px' }}
             navbarScroll
-          >
-            <Nav.Link href="#action1">Home</Nav.Link>
-            <Nav.Link href="#action2">Link</Nav.Link>
-            <NavDropdown title="Link" id="navbarScrollingDropdown">
-              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action4">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action5">
-                Something else here
-              </NavDropdown.Item>
-            </NavDropdown>
-            <Nav.Link href="#" disabled>
-              Link
-            </Nav.Link>
-          </Nav> */}
+          > 
+          <Image src={Logo} className={Style.imgLogo} onClick={Home} alt="" />
+          </Nav>
           
            <div className={Style.divIconesMenu}>
     <FaEnvelope className={Style.iconeNotificacao} Tootlip="Notificações"/>
