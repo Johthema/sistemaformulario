93a6cf91a4893fb57b25aa78b5ff7480dd44a270
Johthema <mauriciojodi@gmail.com>
2023-11-13 14:23:43 -0400

    Atualiza versao cadastro form em andamento

--- src/pages/formularios/cadastro/index.jsx
+++ src/pages/formularios/cadastro/index.jsx
@@ -1,9 +1,37 @@
 import Layout from '../../../components/index';
+import Style from './cadastro.module.css';
+import Form from 'react-bootstrap/Form';
+import {useState, useEffect, useRef} from 'react';
+import { useRouter } from "next/router";
+
 
 export default function CreateForm(){
+
+    const router = useRouter();
+    const [transicao, setTransicao] = useState(false)
+    const inputRef = useRef(null);
+  
+  
+    useEffect(() => {
+      // Ativa a animação após o componente ser montado no cliente
+      setTransicao(true);
+      inputRef.current.focus();
+    }, []);
+
+
+
+
     return(
         <Layout>
-<h5>Tela de cadastro de formulario</h5>
+            <div className={Style.divFundo}>
+
+                <div className={Style.divTituloForm} >
+                    <h5 className={transicao ? Style.nomeFormulario : Style.nomeFormulario2  }>Você precisa de um títuto para seu formuário</h5>
+                    <Form.Control type="text" id="inputPassword5" aria-describedby="passwordHelpBlock" ref={inputRef}/>
+                </div>
+
+
+            </div>
         </Layout>
 
         
