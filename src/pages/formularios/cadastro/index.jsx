c6ad562e1c6d96ed85e06c6e3a8c6d439287f065
Johthema <mauriciojodi@gmail.com>
2023-11-13 15:07:46 -0400

    Atualiza versao

--- src/pages/formularios/cadastro/index.jsx
+++ src/pages/formularios/cadastro/index.jsx
@@ -3,7 +3,7 @@ import Style from './cadastro.module.css';
 import Form from 'react-bootstrap/Form';
 import {useState, useEffect, useRef} from 'react';
 import { useRouter } from "next/router";
-
+import Button from 'react-bootstrap/Button';
 
 export default function CreateForm(){
 
@@ -26,10 +26,16 @@ export default function CreateForm(){
             <div className={Style.divFundo}>
 
                 <div className={Style.divTituloForm} >
-                    <h5 className={transicao ? Style.nomeFormulario : Style.nomeFormulario2  }>Você precisa de um títuto para seu formuário</h5>
-                    <Form.Control type="text" id="inputPassword5" aria-describedby="passwordHelpBlock" ref={inputRef}/>
+                <h5 className={transicao ? Style.nomeFormulario : Style.nomeFormulario2  }>Você precisa de um títuto para seu formulário</h5>
+                    <div className={Style.divFormNome}>
+                        <Form.Control type="text" id="inputPassword5" aria-describedby="passwordHelpBlock" ref={inputRef}/>
+                        <Button variant="primary" className={Style.botaoSeguir1}>Prosseguir</Button>
+                    </div>
+                    
+                    
+                   
                 </div>
-
+                
 
             </div>
         </Layout>
