e5d685fde6eb58419f8e1fadcc6c136c85231039
Johthema <mauriciojodi@gmail.com>
2023-11-14 15:27:18 -0400

    Atualiza versao

---
+++ src/pages/area_pesquisador/formulario/index.jsx
@@ -0,0 +1,12 @@
+import Layout from '../../../components/index';
+
+
+export default function Formulario(){
+    return (
+        <Layout>
+            <h5>Area do pesquisador</h5>
+        </Layout>
+        
+        
+    )
+}
